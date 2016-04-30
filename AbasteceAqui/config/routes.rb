7142e033ad24c6de2c32e25d910d75370155c5fc
Ygor Galeno <ygor.tgaleno@gmail.com>
2016-04-30 03:19:02 -0300

    applying bootstrap on home page

--- AbasteceAqui/config/routes.rb
+++ AbasteceAqui/config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
+
+
   get '/parser/run' => 'parser#import'
   get '/calculator' => 'calculator#is_ethanol_more_profitable?'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
