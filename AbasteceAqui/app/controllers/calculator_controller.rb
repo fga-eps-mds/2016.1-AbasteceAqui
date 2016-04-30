0366246eb23e55d2ad43781c21abadacf470b01b
Anderson Silva <andersoonn.ams@gmail.com>
2016-04-29 22:49:25 -0300

    notes for corrections

--- AbasteceAqui/app/controllers/calculator_controller.rb
+++ AbasteceAqui/app/controllers/calculator_controller.rb
@@ -1,19 +1,23 @@
 class CalculatorController < ApplicationController
-
+	#Vai pra model ou helper!!!
+	#Atualizar o diagrama de pacotes 
+	#As variaveis de precos são locais ou instâncias ?
+	#Receber os preços como parametros
+	
 	def is_ethanol_more_profitable?
 		@ethanol_price = params[:ethanol_price].to_f
 		@gas_price = params[:gas_price].to_f
 
 		profitability_calculation = @ethanol_price / @gas_price
 
-		@a = nil
+		@answer = nil
 		
 		if profitability_calculation <= 0.7
-			@a = true
+			@answer = true
 		elsif profitability_calculation > 0.7
-			@a = false
+			@answer = false
 		end
 
-		return @a
+		return @answer
 	end
 end
