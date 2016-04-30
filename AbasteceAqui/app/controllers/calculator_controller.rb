2b2a74843ee05804dce194d71590d8fa7a596c98
Marco Kuroiva <marco.antoniorl10@gmail.com>
2016-04-30 15:16:05 -0300

    replacing method to model and refactoring calculator_controller and index

--- AbasteceAqui/app/controllers/calculator_controller.rb
+++ AbasteceAqui/app/controllers/calculator_controller.rb
@@ -3,21 +3,11 @@ class CalculatorController < ApplicationController
 	#Atualizar o diagrama de pacotes 
 	#As variaveis de precos são locais ou instâncias ?
 	#Receber os preços como parametros
-	
-	def is_ethanol_more_profitable?
-		@ethanol_price = params[:ethanol_price].to_f
-		@gas_price = params[:gas_price].to_f
+	def index
+		ethanol_price = params[:ethanol_price].to_f
+		gas_price = params[:gas_price].to_f
 
-		profitability_calculation = @ethanol_price / @gas_price
-
-		@answer = nil
-		
-		if profitability_calculation <= 0.7
-			@answer = true
-		elsif profitability_calculation > 0.7
-			@answer = false
-		end
-
-		return @answer
+		@more_profitable = Calculator.calculate(ethanol_price, gas_price)
 	end
+
 end
--- AbasteceAqui/config/routes.rb
+++ AbasteceAqui/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
 
   get '/parser/run' => 'parser#import'
-  get '/calculator' => 'calculator#is_ethanol_more_profitable?'
+  get '/calculator' => 'calculator#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
