992ba59a79a1c6794ea2c7c8d3ea0a4a7f9bea56
Ygor Galeno <ygor.tgaleno@gmail.com>
2016-04-30 03:18:20 -0300

    applying transparent navbar on header

--- AbasteceAqui/app/assets/javascripts/bootstrap.js
+++ AbasteceAqui/app/assets/javascripts/bootstrap.js
@@ -2361,3 +2361,52 @@ if (typeof jQuery === 'undefined') {
   })
 
 }(jQuery);
+
+
+  function main() {
+
+  (function () {
+     'use strict';
+
+
+      /*====================================
+      Show Menu on Book
+      ======================================*/
+
+      $(document).ready(function() {
+      /*var welcome = document.getElementById('tf-home');*/
+      if (document.getElementById('tf-home')  == null)
+      {
+        $('.navbar-default').addClass('on');
+      }
+      else
+      {
+        $('.navbar-default').removeClass('on');
+      
+
+        $(window).bind('scroll', function() {
+            var navHeight = $(window).height() - 580;
+            if ($(window).scrollTop() > navHeight) {
+                $('.navbar-default').addClass('on');
+            } else {
+                $('.navbar-default').removeClass('on');
+            }
+        });
+
+        }
+      });
+
+      $('body').scrollspy({ 
+          target: '.navbar-default',
+          offset: 80
+      })
+
+      $(document).ready(function() {
+
+      });
+
+
+  }());
+
+  }
+  main();
