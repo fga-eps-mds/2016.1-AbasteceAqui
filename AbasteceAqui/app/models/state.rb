59e187a93f3ae1c8013eb67c3460682165627914
Thiago Lima <thiagolima.unb@gmail.com>
2016-05-05 08:47:09 -0300

    creating methods of state.rb

--- AbasteceAqui/app/models/state.rb
+++ AbasteceAqui/app/models/state.rb
@@ -3,4 +3,33 @@ class State < ActiveRecord::Base
 	belongs_to :region
 	has_many :counties
 
+	def fill_states
+
+		@object_states = State.all
+
+	end
+
+	def initialize
+
+		@state_searched = "Digite o estado"
+		@states = []
+
+		@object_states.each do |state|
+			@states << state.name
+		end
+
+	end
+
+	def seach_state_id
+
+		@state_id = Hash.new
+
+		@object_states.each do |state|
+			if state.name == @state_searched
+				@state_id[@state_searched] = state.id
+			end
+		end
+
+	end
+
 end
