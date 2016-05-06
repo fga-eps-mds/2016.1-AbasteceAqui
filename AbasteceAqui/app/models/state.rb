0fcdec15871eb1b79c6b39bc6d7131cb62cb545a
Marco Kuroiva <marco.antoniorl10@gmail.com>
2016-05-06 01:10:06 -0300

    refactoring state

--- AbasteceAqui/app/models/state.rb
+++ AbasteceAqui/app/models/state.rb
@@ -3,15 +3,11 @@ class State < ActiveRecord::Base
 	belongs_to :region
 	has_many :counties
 
-	def initialize
-
-		@states_name = []
-
-	end
 
 	def self.fill_states
 
 		fill_object_states()
+		@states_name = []
 
 		@object_states.each do |state|
 			@states_name << state.name
@@ -21,7 +17,8 @@ class State < ActiveRecord::Base
 
 	end
 
-	def fill_object_states
+	def self.fill_object_states
+
 
 		@object_states = State.all
 
@@ -29,15 +26,14 @@ class State < ActiveRecord::Base
 
 	def self.search_state_counties(state_searched)
 
+		state_searched ||= "SAO PAULO"
 		state_counties = State.find_by(name: state_searched).counties
 
 		counties_of_state = []
 
 		state_counties.each do |counties|
-			counties_of_state = counties.name
+			counties_of_state << counties.name
 		end
-
-		counties_of_state.sort!
 
 		return counties_of_state
 
