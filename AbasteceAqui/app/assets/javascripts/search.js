8a2789d4a3480d40cc1bdb5cc946f5f450c19ed7
Marcelo <marceloabk@hotmail.com>
2016-05-07 14:20:43 -0300

    create a js function to generate random colors in hexa

---
+++ AbasteceAqui/app/assets/javascripts/search.js
@@ -0,0 +1,8 @@
+function get_random_color() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.round(Math.random() * 15)];
+    }
+    return color;
+}
