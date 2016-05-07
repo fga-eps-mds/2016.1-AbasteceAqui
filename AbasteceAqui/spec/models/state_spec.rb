2d92615da1deafeada7b6f982d13413fda9dfbd8
Thiago Lima <thiagolima.unb@gmail.com>
2016-05-07 13:43:10 -0300

    creating test for fill_object_states method

--- AbasteceAqui/spec/models/state_spec.rb
+++ AbasteceAqui/spec/models/state_spec.rb
@@ -6,9 +6,23 @@ RSpec.describe State, type: :model do
 
 		state1 = State.new(id: 1, name: "PARA", region_id: 1)
 		state2 = State.new(id: 2, name: "MARANHAO", region_id: 2)
+		state3 = State.new(id: 3, name: "DISTRITO FEDERAL", region_id: 4)
 
 		state1.save
 		state2.save
+		state3.save
+
+	end
+
+	describe "#fill_states" do
+
+		it "Ordered state names" do
+
+			state_names = State.fill_states
+
+			expect(state_names).to eql(["DISTRITO FEDERAL","MARANHAO", "PARA"])
+
+		end
 
 	end
 
@@ -16,9 +30,8 @@ RSpec.describe State, type: :model do
 
 		it "Find 2 states" do
 
-			State.fill_object_states
-
-			expect((State.fill_object_states).count).to eq(2)
+			expect((State.fill_object_states).count).to eq(3)
+			
 		end
 
 	end
