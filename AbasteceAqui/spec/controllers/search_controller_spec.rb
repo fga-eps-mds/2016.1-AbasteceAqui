829226b262d1a1cf42a2ccef8e6c5cfba23983fc
Anderson <andersoonn.ams@gmail.com>
2016-05-07 19:03:29 -0300

    creating test for find_researches_of_county method

--- AbasteceAqui/spec/controllers/search_controller_spec.rb
+++ AbasteceAqui/spec/controllers/search_controller_spec.rb
@@ -14,15 +14,14 @@ RSpec.describe SearchController, type: :controller do
 
 
 		fuel_research1 = FuelResearch.new(id:1, date:"2016-01-01", county_id: 1)
+		fuel_research2 = FuelResearch.new(id:2, date:"2016-02-01", county_id: 1)
+		fuel_research3 = FuelResearch.new(id:3, date:"2016-03-01", county_id: 1)
+
 
 		fuel1 = Fuel.new(id: 1, number_of_gas_station: 4, min_resale_price: 2.199, 
 			medium_resale_price: 2.199, max_resale_price: 2.199, resale_standard_deviation: 0.0, 
 			min_distribuition_price: 0.0, medium_distribuition_price: 0.0, max_distribuition_price: 0.0, 
 			distribuition_standard_deviation: 0.0, fuel_research_id: 1, fuel_type_id: 6)
-
-
-
-
 
 		state1.save
 		state2.save
@@ -32,6 +31,8 @@ RSpec.describe SearchController, type: :controller do
 		county2.save
 
 		fuel_research1.save
+		fuel_research2.save
+		fuel_research3.save
 
 		fuel1.save
 
@@ -69,7 +70,16 @@ RSpec.describe SearchController, type: :controller do
   	end
   end
 =end
-	
+
+
+	describe "#find_researches_of_county" do
+		it "Should return the id of the last county searched" do
+			last_id = controller.find_researches_of_county("BRASILIA")
+			expect(last_id).to eq(3)
+		end
+
+	end
+
 	describe "#find_date_last_research" do
 		it "Should return the last date research" do
 			date = controller.find_date_last_research(1)
