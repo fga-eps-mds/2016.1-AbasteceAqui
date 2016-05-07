8ee820f428e22b23f39b866d7a21e6ad35b8f72f
Anderson <andersoonn.ams@gmail.com>
2016-05-07 17:39:12 -0300

    creating test for search_fuels_last_research method

--- AbasteceAqui/spec/controllers/search_controller_spec.rb
+++ AbasteceAqui/spec/controllers/search_controller_spec.rb
@@ -8,9 +8,32 @@ RSpec.describe SearchController, type: :controller do
 		state2 = State.new(id: 2, name: "MARANHAO", region_id: 2)
 		state3 = State.new(id: 3, name: "DISTRITO FEDERAL", region_id: 3);
 		
+		county1 = County.new(id: 1, name: "BRASILIA", state_id: 3)
+		county2 = County.new(id: 2, name: "GAMA", state_id: 3)
+
+
+
+		fuel_research1 = FuelResearch.new(id:1, date:"2016-01-01", county_id: 1)
+
+		fuel1 = Fuel.new(id: 1, number_of_gas_station: 4, min_resale_price: 2.199, 
+			medium_resale_price: 2.199, max_resale_price: 2.199, resale_standard_deviation: 0.0, 
+			min_distribuition_price: 0.0, medium_distribuition_price: 0.0, max_distribuition_price: 0.0, 
+			distribuition_standard_deviation: 0.0, fuel_research_id: 1, fuel_type_id: 6)
+
+
+
+
 		state1.save
 		state2.save
 		state3.save
+
+		county1.save
+		county2.save
+
+		fuel_research1.save
+
+		fuel1.save
+
 	end
 
   describe "#find_states" do
@@ -20,6 +43,39 @@ RSpec.describe SearchController, type: :controller do
       expect(states).to eql(["DISTRITO FEDERAL", "MARANHAO", "PARA"])
     end
   end
-  
+
+
+	
+
+=begin
+
+  describe "#find_counties_of_state" do
+
+  	before do
+
+  	get :index, {:state_searched => "DISTRITO FEDERAL", :county_searched => "GAMA"} 
+
+  	end
+  	it "Should be the counties of the state" do
+
+   			state_searched = "DISTRITO FEDERAL"
+
+   			counties_of_state = controller.find_counties_of_state #, {:state_searched => "DISTRITO FEDERAL"}
+  			
+   		expect(counties_of_state).to eql(["BRASILIA", "GAMA"])
+  		
+  		
+  	end
+  end
+=end
+
+
+	describe "#search_fuels_last_research" do
+		it "Should return one object of array" do
+			fuel = controller.search_fuels_last_research(1)
+			expect(fuel.count).to eq(1)
+		end
+	end
+ 
 
 end
