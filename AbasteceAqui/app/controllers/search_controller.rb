1b5282d549d2564eb44bbcdd74c6f8f2631761b5
Anderson <andersoonn.ams@gmail.com>
2016-05-07 17:36:31 -0300

    refactoring search_controller

--- AbasteceAqui/app/controllers/search_controller.rb
+++ AbasteceAqui/app/controllers/search_controller.rb
@@ -4,11 +4,11 @@ class SearchController < ApplicationController
 
 		find_states()
 		@state_searched = params[:state_searched]
-		find_counties_of_state()
+		find_counties_of_state(@state_searched)
 		@county_searched = params[:county_searched]
-		find_researches_of_county()
-		find_date_last_research()
-		search_fuels_last_research()
+		find_researches_of_county(@county_searched)
+		find_date_last_research(@id_last_research)
+		search_fuels_last_research(@id_last_research)
 
 	end
 
@@ -18,27 +18,27 @@ class SearchController < ApplicationController
 		
 	end
 
-	def find_counties_of_state
+	def find_counties_of_state(state_searched)
 		
-		@counties_of_state = State.search_state_counties(@state_searched)
+		@counties_of_state = State.search_state_counties(state_searched)
 
 	end
 
-	def find_researches_of_county
+	def find_researches_of_county(county_searched)
 
-		@id_last_research = County.search_county_research(@county_searched)
+		@id_last_research = County.search_county_research(county_searched)
 
 	end
 
-	def find_date_last_research 
+	def find_date_last_research(id_last_research) 
 
-		@date = FuelResearch.find_by(id: @id_last_research).date
+		@date = FuelResearch.find_by(id: id_last_research).date
 	
 	end
 
-	def search_fuels_last_research
+	def search_fuels_last_research(id_last_research)
 
-		@fuel = FuelResearch.search_fuels_research(@id_last_research)
+		@fuel = FuelResearch.search_fuels_research(id_last_research)
 	
 	end
 
