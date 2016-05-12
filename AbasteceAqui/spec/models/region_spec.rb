8adf699e3a4e12dfdce7b2524ba92b35217f9ed2
Thiago Lima <thiagolima.unb@gmail.com>
2016-05-12 02:47:38 -0300

    refactoring tests for fuel_type and region classes

--- AbasteceAqui/spec/models/region_spec.rb
+++ AbasteceAqui/spec/models/region_spec.rb
@@ -2,12 +2,25 @@ require "rails_helper"
 
 RSpec.describe Region, type: :model do 
 
-	describe "#new" do
+	before do
 
-		it "Should be a instance of Region" do
-			region = Region.new
+		region = Region.new(id: 1, name: "SUL")
+		region.save
 
-			expect(region).to be_an_instance_of Region
+		state1 = State.new(id: 1, name: "PARANA", region_id: 1)
+		state2 = State.new(id: 2, name: "RIO GRANDE DO SUL", region_id: 1)
+		state1.save
+		state2.save
+
+	end
+
+
+	describe "#.states" do
+
+		it "Should return number of states of a region" do
+			states = Region.find(1).states
+
+			expect(states.count).to eq(2)
 
 		end
 
