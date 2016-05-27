1ba9d91eeb5f271c0724d97446a7c8dd1bdbafa1
Marcelo <marceloabk@hotmail.com>
2016-05-27 17:09:06 -0300

    refactoring and document map_routes.js

--- AbasteceAqui/app/assets/javascripts/map_routes.js
+++ AbasteceAqui/app/assets/javascripts/map_routes.js
@@ -1,28 +1,38 @@
+// initialize the map
 function initMap() {
-	var directionsDisplay = new google.maps.DirectionsRenderer;
-	var directionsService = new google.maps.DirectionsService;  
 
-	var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -33.7217175, lng: -55.0783226},
+	const directionsDisplay = new google.maps.DirectionsRenderer;
+	const directionsService = new google.maps.DirectionsService;  
+  const brazilCoords = new google.maps.LatLng(-13, -55);
+
+  // create a map centered in brazil  
+	const map = new google.maps.Map(document.getElementById('map-container'), {
+    center: brazilCoords,
     zoom: 4
   });
+
+  // reference to how directions renderer works
+  // https://developers.google.com/maps/documentation/javascript/directions#RenderingDirections
   directionsDisplay.setMap(map);
 
-  var onChangeHandler = function() {
-    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  // callback to when the user type a origin and a destination
+  const onChangeHandler = function() {
+    calculateAndDisplayRoute(directionsService, directionsDisplay, map);
   };
 
-  var origin = document.getElementById('origin');
-  var destination = document.getElementById('destination');
-
-  var originBox = new google.maps.places.SearchBox(origin);
-  var destinationBox = new google.maps.places.SearchBox(destination);
-
+  // create the search boxes and link them to the UI elements.
+  const origin = document.getElementById('origin');
+  const originBox = new google.maps.places.SearchBox(origin);
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(origin);
+
+  const destination = document.getElementById('destination');
+  const destinationBox = new google.maps.places.SearchBox(destination);
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(destination);
 
+  // updated boxes limits
   map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
+    originBox.setBounds(map.getBounds());
+    destinationBox.setBounds(map.getBounds());
   });
 
   originBox.addListener('places_changed', onChangeHandler);
@@ -30,15 +40,18 @@ function initMap() {
 }
 
 
-function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+// calculate and display a route
+function calculateAndDisplayRoute(directionsService, directionsDisplay, map) {
 
 	var origin = document.getElementById('origin').value;
   var destination = document.getElementById('destination').value;
 
+  // check if user typed a origin and a destination
   if (origin.length === 0 || destination.length === 0) {
   	return;
   }
 
+  // calc and display the route
   directionsService.route({
     origin: origin,
     destination: destination,
