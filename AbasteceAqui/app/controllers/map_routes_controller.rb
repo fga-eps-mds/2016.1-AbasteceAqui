2af00418c63470c0951c0e3033756e2e8864ff32
Marcelo <marceloabk@hotmail.com>
2016-05-31 19:02:45 -0300

    sending all needed data to the view (view rendering too slow)

--- AbasteceAqui/app/controllers/map_routes_controller.rb
+++ AbasteceAqui/app/controllers/map_routes_controller.rb
@@ -2,13 +2,52 @@ class MapRoutesController < ApplicationController
 
 	def index
 
-		find_counties()
+		@counties  = find_counties()
+		@researches = find_last_research_of_counties(@counties)
+		@fuels = find_fuels_of_research(@researches)
 
 	end
 	
 	def find_counties
 
-		@counties = County.fill_counties
+		counties = County.fill_counties
+
+		return counties
 
 	end
+
+	def find_last_research_of_counties(counties)
+
+		researches = []
+
+		for county in counties
+			researches << FuelResearch.search_fuels_research(county)
+		end
+
+		return researches
+
+	end
+
+	def find_fuels_of_research(researches)
+
+		fuels = []
+
+		for research in researches
+
+			for fuel in research
+
+				name = fuel.fuel_type.type_name
+				fuel_hash = Hash.new
+				fuel_hash[name] = research
+				fuels << fuel_hash
+
+			end
+
+		end
+
+		return fuels
+
+	end
+
+
 end
