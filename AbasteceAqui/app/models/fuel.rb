6af0691b2a5e2904a469f652f7e78a41808aeef9
Marcelo <marceloabk@hotmail.com>
2016-06-04 15:33:46 -0300

    refactoring fuels_latest_researches_counties to fuel model

--- AbasteceAqui/app/models/fuel.rb
+++ AbasteceAqui/app/models/fuel.rb
@@ -3,4 +3,25 @@ class Fuel < ActiveRecord::Base
 	belongs_to :fuel_research
 	belongs_to :fuel_type
 
+	def self.fuels_latest_researches_counties(researches)
+
+		fuels = []
+
+		for research in researches
+
+			for fuel in research.fuels
+
+				name = fuel.fuel_type.type_name
+				fuel_hash = Hash.new
+				fuel_hash[name] = research
+				fuels << fuel_hash
+
+			end
+
+		end
+
+		return fuels
+		
+	end
+
 end
