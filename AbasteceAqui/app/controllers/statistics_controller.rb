98a71e9569b77f49a8a0223f6610084e62fbbd23
Victor Fernandes <victor.hmfd@gmail.com>
2016-06-05 16:16:49 -0300

    removing unnecessary method in the county model and set precision in values of fuel

--- AbasteceAqui/app/controllers/statistics_controller.rb
+++ AbasteceAqui/app/controllers/statistics_controller.rb
@@ -13,7 +13,7 @@ class StatisticsController < ApplicationController
 		if @state_searched != nil
 			@counties_of_state = State.search_state_counties(@state_searched)
 		else
-			@state_searched = 'ACRE' 
+			@state_searched = 'ACRE'
 			@counties_of_state = @SearchController.find_counties_of_state(@state_searched)
 		end
 
@@ -42,18 +42,18 @@ class StatisticsController < ApplicationController
 
 			f.legend(align: 'right', verticalAlign: 'top', y: 75, x: -50, layout: 'vertical')
 			f.chart({defaultSeriesType: "line"})
-		end			
+		end
 	end
 
 	def fuels_of_year
 
 		@researches_of_county = County.find_by(name: @county_searched).fuel_researches
-		@years_of_researches = Hash.new 
+		@years_of_researches = Hash.new
 
 		@years.each do |year|
 
 			@years_of_researches[year] = Hash.new
-			@researches_of_year = [] 
+			@researches_of_year = []
 
 			@researches_of_county.each do |research|
 
@@ -77,7 +77,7 @@ class StatisticsController < ApplicationController
 
 	#These variables stores the three mediums prices of the three types of fuels.
 	@gas_prices = [] #FuelType id: 2
-	@ethanol_prices = [] #FuelType id: 1 
+	@ethanol_prices = [] #FuelType id: 1
 	@diesel_prices = [] #FuelType id: 5
 
 	fuels_of_year.each do |year, fuels|
@@ -92,14 +92,14 @@ class StatisticsController < ApplicationController
 		gas_price_div = 0.0
 		diesel_price_div = 0.0
 		ethanol_price_div = 0.0
-		
+
 
 		fuels.each do |fuel|
 
 			if fuel.fuel_type_id == 1
 				@ethanol_prices_month << fuel
 			elsif fuel.fuel_type_id == 2
-				@gas_prices_month << fuel 
+				@gas_prices_month << fuel
 			elsif fuel.fuel_type_id == 5
 				@diesel_prices_month << fuel
 			end
@@ -129,16 +129,16 @@ class StatisticsController < ApplicationController
 			end
 		end
 
-		@gas_prices << sum_gas / gas_price_div
-		@ethanol_prices << sum_ethanol / ethanol_price_div
-		@diesel_prices << sum_diesel / diesel_price_div
+		@gas_prices << (sum_gas / gas_price_div).round(3)
+		@ethanol_prices << (sum_ethanol / ethanol_price_div).round(3)
+		@diesel_prices << (sum_diesel / diesel_price_div).round(3)
 
 
 	end
 
 	@total_price = [@gas_prices, @ethanol_prices, @diesel_prices]
 
-	return @total_price 
+	return @total_price
 end
 
 end
