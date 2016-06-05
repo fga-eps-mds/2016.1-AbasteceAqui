f0ac627d23ea61a9e0ca6438495d101a1c4ce431
Victor Fernandes <victor.hmfd@gmail.com>
2016-06-05 19:23:25 -0300

    creating test for method county_anual in statistics controlle and removing unnecessary files

--- AbasteceAqui/spec/controllers/statistics_controller_spec.rb
+++ AbasteceAqui/spec/controllers/statistics_controller_spec.rb
@@ -2,11 +2,32 @@ require 'rails_helper'
 
 RSpec.describe StatisticsController, type: :controller do
 
+  before do
+    @state1 = State.new(id: 1, name: "DISTRITO FEDERAL");
+    @county1 = County.new(id: 1, name: "BRASILIA", state_id: 1)
+
+    @state1.save
+    @county1.save
+
+  end
+
   describe "GET #index" do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "GET #county_anual" do
+    it "returns http success when state_searched != nil" do
+      get :county_anual, {:state_searched => @state1.name}
+      expect(response).to have_http_status(:success)
+  end
+
+  it "returns http success when county_searched != nil" do
+    get :county_anual, {:county_searched => @county1.name}
+    expect(response).to have_http_status(:success)
+  end
+end
 
 end
