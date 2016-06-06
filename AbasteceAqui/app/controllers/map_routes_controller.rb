94c7e57c67527591c74f0776cda3eb94f1891aaa
Ygor Galeno <ygor.tgaleno@gmail.com>
2016-06-06 14:30:10 -0300

    refactoring map_routes controller, and doing the respective test

--- AbasteceAqui/app/controllers/map_routes_controller.rb
+++ AbasteceAqui/app/controllers/map_routes_controller.rb
@@ -1,17 +1,26 @@
 class MapRoutesController < ApplicationController
 
 	def index
+		
+	end
+
+	def index_database
+
+		find_counties()
+		find_last_research_of_counties(@counties)
+		find_states()
+		find_fuels_of_research(@researches)
 
 	end
 
-	def find_states 
+	def find_states
 
 		@states = State.fill_object_states()
 
 		return @states
 
 	end
-	
+
 	def find_counties
 
 		@counties = County.fill_counties()
@@ -20,21 +29,17 @@ class MapRoutesController < ApplicationController
 
 	end
 
-	def find_last_research_of_counties
+	def find_last_research_of_counties(counties)
 
-		@researches = FuelResearch.fill_object_last_research(@counties)
+		@researches = FuelResearch.fill_object_last_research(counties)
 
 		return @researches
 
 	end
 
-	def find_fuels_of_research
+	def find_fuels_of_research(researches)
 
-		find_counties()
-		find_last_research_of_counties()
-		find_states()
-
-		@fuels = Fuel.fuels_latest_researches_counties(@researches)
+		@fuels = Fuel.fuels_latest_researches_counties(researches)
 
 		return @fuels
 
