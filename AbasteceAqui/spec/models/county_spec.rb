7d78a5c9fcb1313495cb25f380f493d54eb305fc
Victor Fernandes <victor.hmfd@gmail.com>
2016-06-06 17:34:38 -0300

    creating tests for new methods in models Fuel, FuelResearch and County

--- AbasteceAqui/spec/models/county_spec.rb
+++ AbasteceAqui/spec/models/county_spec.rb
@@ -1,15 +1,17 @@
 require "rails_helper"
 
-RSpec.describe County, type: :model do 
+RSpec.describe County, type: :model do
 
 	before do
 
 		county1 = County.new(id: 1, name: "ABAETETUBA", state_id: 1)
+		county2 = County.new(id: 2, name: "BRASILIA")
 		fuel_research1 = FuelResearch.new(id: 1, date: "2016-01-01", county_id: 1)
 		fuel_research2 = FuelResearch.new(id: 2, date: "2016-02-01", county_id: 1)
 		fuel_research3 = FuelResearch.new(id: 3, date: "2016-03-01", county_id: 1)
 
 		county1.save
+		county2.save
 		fuel_research1.save
 		fuel_research2.save
 		fuel_research3.save
@@ -17,7 +19,6 @@ RSpec.describe County, type: :model do
 	end
 
 	describe "#search_county_research" do
-
 		it "Find last reserach equal 3" do
 
 			last_research = County.search_county_research("ABAETETUBA")
@@ -25,7 +26,15 @@ RSpec.describe County, type: :model do
 			expect(last_research).to eq(3)
 
 		end
+	end
 
+	describe "#fill_counties" do
+		it "should returns all counties in database" do
+			counties1 = County.all
+			counties2 = County.fill_counties
+
+			expect(counties1).to eq(counties2)
+		end
 	end
 
 end
