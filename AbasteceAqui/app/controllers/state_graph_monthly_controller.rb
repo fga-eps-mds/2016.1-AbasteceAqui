5a3c8682e5e6ea8448927eed9ec9e04d30233692
Thiago Lima <thiagolima.unb@gmail.com>
2016-06-08 21:42:04 -0300

    creating drop down of states in US04

--- AbasteceAqui/app/controllers/state_graph_monthly_controller.rb
+++ AbasteceAqui/app/controllers/state_graph_monthly_controller.rb
@@ -1,3 +1,18 @@
 class StateGraphMonthlyController < ApplicationController
 
+	def state_monthly
+
+		@states = State.fill_states
+		@state_searched = params[:state_searched]
+
+		if @state_searched != nil
+			@counties_of_state = State.search_state_counties(@state_searched)
+		else
+			# do nothing
+		end
+
+		@county_searched = params[:county_searched]
+
+	end
+
 end
