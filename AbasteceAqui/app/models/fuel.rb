5ac11350fb8fc642c0399669da7d873d588719a3
Ygor Galeno <ygor.tgaleno@gmail.com>
2016-06-09 04:25:33 -0300

    creating method to find county fuels of last research

--- AbasteceAqui/app/models/fuel.rb
+++ AbasteceAqui/app/models/fuel.rb
@@ -18,7 +18,47 @@ class Fuel < ActiveRecord::Base
 		end
 
 		return fuels
-		
+
+	end
+
+	def self.county_fuels_last_research(researches)
+
+		counties_fuels = []
+		gasoline_prices = []
+		alcool_prices = []
+		diesel_prices = []
+
+		researches.each do |research|
+
+			if(research.fuels.find_by(fuel_type_id: 1) != nil)
+				alcool_prices << research.fuels.find_by(fuel_type_id: 1)
+			else
+				#do nothing
+			end
+			if(research.fuels.find_by(fuel_type_id: 2) != nil)
+				gasoline_prices << research.fuels.find_by(fuel_type_id: 2)
+			else
+				#do nothing
+			end
+
+			if(research.fuels.find_by(fuel_type_id: 5) != nil)
+				diesel_prices << research.fuels.find_by(fuel_type_id: 5)
+			else
+				#do nothing
+			end
+
+		end
+
+		alcool_prices.sort_by! {|cheaper| cheaper.min_resale_price}
+		gasoline_prices.sort_by! {|cheaper| cheaper.min_resale_price}
+		diesel_prices.sort_by! {|cheaper| cheaper.min_resale_price}
+
+		counties_fuels[0] = alcool_prices
+		counties_fuels[1] = gasoline_prices
+		counties_fuels[2] = diesel_prices
+
+		return counties_fuels
+
 	end
 
 end
