7c77955b9686977fb7ee7f77fc90c9e5cbed63c3
Ygor Galeno <ygor.tgaleno@gmail.com>
2016-06-09 04:29:15 -0300

    creating method to sort counties by fuel price

--- AbasteceAqui/app/models/county.rb
+++ AbasteceAqui/app/models/county.rb
@@ -27,4 +27,27 @@ class County < ActiveRecord::Base
 
 	end
 
+	def self.sort_counties_by_fuel_price(sorted_research)
+
+		sorted_counties = []
+
+		sorted_counties[0] = []
+		for i in 0..(sorted_research[0].length-1)
+			sorted_counties[0] << County.find_by(id: sorted_research.county_id).name
+		end
+
+		sorted_counties[1] = []
+		for i in 0..(sorted_research[1].length-1)
+			sorted_counties[1] << County.find_by(id: sorted_research.county_id).name
+		end
+
+		sorted_counties[2] = []
+		for i in 0..(sorted_research[2].length-1)
+			sorted_counties[2] << County.find_by(id: sorted_research.county_id).name
+		end
+
+		return sorted_counties
+
+	end
+
 end
