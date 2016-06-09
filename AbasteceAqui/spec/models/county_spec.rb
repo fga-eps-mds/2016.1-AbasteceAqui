7208dc171aa58756b5fb8429e3d6e7c748af111a
Anderson Silva <andersoonn.ams@gmail.com>
2016-06-09 20:05:59 -0300

    creating test for the new methods of county model

--- AbasteceAqui/spec/models/county_spec.rb
+++ AbasteceAqui/spec/models/county_spec.rb
@@ -10,11 +10,34 @@ RSpec.describe County, type: :model do
 		fuel_research2 = FuelResearch.new(id: 2, date: "2016-02-01", county_id: 1)
 		fuel_research3 = FuelResearch.new(id: 3, date: "2016-03-01", county_id: 1)
 
+		@fuel1 = Fuel.new(id: 1, number_of_gas_station: 4,
+         min_resale_price: 2.199, medium_resale_price: 2.199,
+         max_resale_price: 2.199, resale_standard_deviation: 0.0,
+         min_distribuition_price: 0.0, medium_distribuition_price: 0.0,
+         max_distribuition_price: 0.0, distribuition_standard_deviation: 0.0,
+         fuel_research_id: 1, fuel_type_id: 1)
+    @fuel2 = Fuel.new(id: 2, number_of_gas_station: 4,
+         min_resale_price: 2.199, medium_resale_price: 2.199,
+         max_resale_price: 2.199, resale_standard_deviation: 0.0,
+         min_distribuition_price: 0.0, medium_distribuition_price: 0.0,
+         max_distribuition_price: 0.0, distribuition_standard_deviation: 0.0,
+         fuel_research_id: 1, fuel_type_id: 2)
+
+    @fuel3 = Fuel.new(id: 3, number_of_gas_station: 4,
+         min_resale_price: 2.199, medium_resale_price: 2.199,
+         max_resale_price: 2.199, resale_standard_deviation: 0.0,
+         min_distribuition_price: 0.0, medium_distribuition_price: 0.0,
+         max_distribuition_price: 0.0, distribuition_standard_deviation: 0.0,
+         fuel_research_id: 2, fuel_type_id: 5)
+
 		county1.save
 		county2.save
 		fuel_research1.save
 		fuel_research2.save
 		fuel_research3.save
+		@fuel1.save
+    @fuel2.save
+    @fuel3.save
 
 	end
 
@@ -36,5 +59,19 @@ RSpec.describe County, type: :model do
 			expect(counties1).to eq(counties2)
 		end
 	end
+
+	describe "#fill_counties" do
+		it "should returns all counties in database" do
+
+			counties_fuels = []
+			counties_fuels[0] = [@fuel1, @fuel2, @fuel3]
+			counties_fuels[1] = [@fuel1, @fuel2, @fuel3]
+			counties_fuels[2] = [@fuel1, @fuel2, @fuel3]
+
+			sorted_counties = County.sort_counties_by_fuel_price(counties_fuels)
+			expect(sorted_counties.count).to eq(3)
+		end
+	end
+
 
 end
