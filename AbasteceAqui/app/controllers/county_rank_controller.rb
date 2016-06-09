3e12bffea4a183c1e6daf8af342abc699251e797
Ygor Galeno <ygor.tgaleno@gmail.com>
2016-06-09 04:30:13 -0300

    calling methods from model, in controller of county rank

--- AbasteceAqui/app/controllers/county_rank_controller.rb
+++ AbasteceAqui/app/controllers/county_rank_controller.rb
@@ -6,34 +6,53 @@ class CountyRankController < ApplicationController
 
   def county_rank_by_date
 
-    fill_object_county()
+    find_counties()
     find_last_research_of_counties(@counties)
-    find_fuels_of_research(@researches)
+    find_county_fuels_last_research(@researches)
+    fill_researches_by_fuel_price(@counties_fuels)
+    fil_counties_by_fuel_price(@sorted_research)
 
   end
 
-  def fill_object_county
 
-    @counties = County.fill_counties();
+  def find_counties
 
-    return @counties
+		@counties = County.fill_counties()
 
-  end
-
-  def find_last_research_county(counties)
-
-    @researches = FuelResearch.fill_object_last_research(counties)
-
-    return @researches
-
-  end
-
-  def find_fuels_of_research(researches)
-
-		@fuels = Fuel.fuels_latest_researches_counties(researches)
-
-		return @fuels
+		return @counties
 
 	end
+
+  def find_last_research_of_counties(counties)
+
+		@researches = FuelResearch.fill_object_last_research(counties)
+
+		return @researches
+
+	end
+
+  def find_county_fuels_last_research(researches)
+
+    @counties_fuels = Fuel.county_fuels_last_research(researches)
+
+    return @counties_fuels
+
+  end
+
+  def fill_researches_by_fuel_price(counties_fuels)
+
+    @sorted_research = FuelResearch.sort_researches_by_fuel_price(counties_fuels)
+
+    return @sorted_research
+
+  end
+
+  def fill_counties_by_fuel_price(sorted_research)
+
+    @sorted_counties = County.sort_counties_by_fuel_price(sorted_research)
+
+    return @sorted_counties
+
+  end
 
 end
