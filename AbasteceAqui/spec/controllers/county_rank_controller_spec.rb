37dfab94bdc2116d83c56cdf06b9592e89f4e66f
Anderson Silva <andersoonn.ams@gmail.com>
2016-06-10 04:28:31 -0300

    doing new test for new methods of controller, and adding route to ethanol action

--- AbasteceAqui/spec/controllers/county_rank_controller_spec.rb
+++ AbasteceAqui/spec/controllers/county_rank_controller_spec.rb
@@ -14,17 +14,17 @@ RSpec.describe CountyRankController, type: :controller do
     @fuel_research1 = FuelResearch.new(id:1, date:"2016-01-01", county_id: 1)
     @fuel_research2 = FuelResearch.new(id:2, date:"2016-02-01", county_id: 2)
     @fuel_research3 = FuelResearch.new(id:3, date:"2016-03-01", county_id: 1)
-    @fuel_research2 = FuelResearch.new(id:4, date:"2016-03-01", county_id: 2)
+    @fuel_research4 = FuelResearch.new(id:4, date:"2016-03-01", county_id: 2)
 
     @fuel1 = Fuel.new(id: 1, number_of_gas_station: 4, min_resale_price: 2.199,
       medium_resale_price: 2.199, max_resale_price: 2.199, resale_standard_deviation: 0.0,
       min_distribuition_price: 0.0, medium_distribuition_price: 0.0, max_distribuition_price: 0.0,
-      distribuition_standard_deviation: 0.0, fuel_research_id: 3, fuel_type_id: 1)
+      distribuition_standard_deviation: 0.0, fuel_research_id: 1, fuel_type_id: 1)
 
     @fuel2 = Fuel.new(id: 2, number_of_gas_station: 4, min_resale_price: 2.199,
       medium_resale_price: 2.199, max_resale_price: 2.199, resale_standard_deviation: 0.0,
       min_distribuition_price: 0.0, medium_distribuition_price: 0.0, max_distribuition_price: 0.0,
-      distribuition_standard_deviation: 0.0, fuel_research_id: 3, fuel_type_id: 2)
+      distribuition_standard_deviation: 0.0, fuel_research_id: 2, fuel_type_id: 2)
 
     @fuel3 = Fuel.new(id: 3, number_of_gas_station: 4, min_resale_price: 2.199,
       medium_resale_price: 2.199, max_resale_price: 2.199, resale_standard_deviation: 0.0,
@@ -34,7 +34,7 @@ RSpec.describe CountyRankController, type: :controller do
     @fuel4 = Fuel.new(id: 4, number_of_gas_station: 4, min_resale_price: 2.199,
       medium_resale_price: 2.199, max_resale_price: 2.199, resale_standard_deviation: 0.0,
       min_distribuition_price: 0.0, medium_distribuition_price: 0.0, max_distribuition_price: 0.0,
-      distribuition_standard_deviation: 0.0, fuel_research_id: 3, fuel_type_id: 5)
+      distribuition_standard_deviation: 0.0, fuel_research_id: 4, fuel_type_id: 5)
 
 
     @county1.save
@@ -43,40 +43,69 @@ RSpec.describe CountyRankController, type: :controller do
     @fuel_research1.save
     @fuel_research2.save
     @fuel_research3.save
+    @fuel_research4.save
+
+    @fuel1.save
+    @fuel2.save
+    @fuel3.save
+    @fuel4.save
 
   end
 
   describe "#find_counties" do
     it "Should return two objects of counties" do
+
       counties = controller.find_counties
       expect(counties.count).to eq(2)
+
     end
   end
 
   describe "#find_last_research_of_counties" do
     it "Should return two objects of last research" do
+
       last_research = controller.find_last_research_of_counties(County.all)
       expect(last_research.count).to eq(2)
+
     end
   end
 
-  describe "#find_county_fuels_last_research" do
-    it "Should return two objects of fuel of last research" do
-      fuels_last_research = controller.find_county_fuels_last_research(FuelResearch.all)
-      expect(fuels_last_research.count).to eq(3)
+  describe "#fill_fuels_last_research" do
+    it "Should return two objects of fuels last research" do
+
+      last_research = controller.find_last_research_of_counties(County.all)
+      fuels_last_research = controller.fill_fuels_last_research(last_research)
+
+      expect(fuels_last_research.count).to eq(2)
+
     end
   end
 
-  describe "#sorted_counties" do
-    it "Should return two objects of fuel of last research" do
-      fuels_last_research = controller.sorted_counties(controller.find_county_fuels_last_research(FuelResearch.all))
-      expect(fuels_last_research.count).to eq(3)
+  describe "#find_ethanol_fuels_sorted" do
+    it "Should return one objects of fuel" do
+
+      fuels = Fuel.all
+      ethanol_sorted = controller.find_ethanol_fuels_sorted(fuels)
+
+      expect(ethanol_sorted.count).to eq(1)
+
     end
   end
 
-  describe "#county_rank_by_date" do
+  describe "#find_counties_sorted_by_fuel" do
+    it "Should return four objects of counties" do
+
+      fuels = Fuel.all
+      counties = controller.find_counties_sorted_by_fuel(fuels)
+
+      expect(counties.count).to eq(4)
+
+    end
+  end
+
+  describe "#county_rank_by_ethanol" do
 		it "Should render database html" do
-			get :county_rank_by_date
+			get :county_rank_by_ethanol
       expect(response).to have_http_status(:success)
     end
 	end
