7d1df04b79fcd0b5fd281ec92f834020db5a6298
Ygor Galeno <ygor.tgaleno@gmail.com>
2016-06-12 04:22:09 -0300

    adding js files, for precompile in production mode

--- AbasteceAqui/config/initializers/assets.rb
+++ AbasteceAqui/config/initializers/assets.rb
@@ -2,6 +2,15 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( smooth_scroll.js )
+Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( jquery-confirm.js )
+Rails.application.config.assets.precompile += %w( map_routes.js )
+Rails.application.config.assets.precompile += %w( calculator.js )
+Rails.application.config.assets.precompile += %w( highcharts-custom.js )
+Rails.application.config.assets.precompile += %w( dinamic_selection.js )
+Rails.application.config.assets.precompile += %w( county_anual.js.erb )
+Rails.application.config.assets.precompile += %w( highcharts-custom.js )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
