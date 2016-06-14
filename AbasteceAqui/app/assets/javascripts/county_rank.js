282f74423a6c18a920ce470a39b07fba098ad989
Ygor Galeno <ygor.tgaleno@gmail.com>
2016-06-14 17:51:23 -0300

    adjusting test environment

--- AbasteceAqui/app/assets/javascripts/county_rank.js
+++ AbasteceAqui/app/assets/javascripts/county_rank.js
@@ -8,13 +8,19 @@ $(document ).ready(function ( ) {
   var ethanol_btn = document.getElementById("ethanol-rank-btn");
   var diesel_btn = document.getElementById("diesel-rank-btn");
 
+  if($(gasoline_btn).hasClass("active")) {
+    document.getElementById("rank-ethanol").style.display = 'none';
+    document.getElementById("rank-diesel").style.display = 'none';
+  }
 
   $(ethanol_btn).click(function () {
 
+    // Removing class active from other tabs, and adding active class for tab selected
     $(diesel_btn).removeClass("active");
     $(gasoline_btn).removeClass("active");
     $(ethanol_btn).addClass('active');
 
+    // Fade in/Fade out effect
     $(diesel).delay(100).fadeOut("slow");
     $(gasoline).delay(100).fadeOut("slow");
     $(ethanol).delay(600).fadeIn("slow");
@@ -22,10 +28,13 @@ $(document ).ready(function ( ) {
   });
 
   $(gasoline_btn).on('click', function () {
+
+    // Removing class active from other tabs, and adding active class for tab selected
     $(diesel_btn).removeClass("active");
     $(ethanol_btn).removeClass("active");
     $(gasoline_btn).addClass('active');
 
+    // Fade in/Fade out effect
     $(diesel).delay(100).fadeOut("slow");
     $(ethanol).delay(100).fadeOut("slow");
     $(gasoline).delay(600).fadeIn("slow");
@@ -33,10 +42,12 @@ $(document ).ready(function ( ) {
 
   $(diesel_btn).click(function () {
 
+    // Removing class active from other tabs, and adding active class for tab selected
     $(gasoline_btn).removeClass("active");
     $(ethanol_btn).removeClass('active');
     $(diesel_btn).addClass("active");
 
+    // Fade in/Fade out effect 
     $(gasoline).delay(100).fadeOut("slow");
     $(ethanol).delay(100).fadeOut("slow");
     $(diesel).delay(600).fadeIn("slow");
