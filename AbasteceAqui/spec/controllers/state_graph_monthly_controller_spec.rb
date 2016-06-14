6d4e64a13609b391d9984d7a6c9f4089effb5d84
Thiago Lima <thiagolima.unb@gmail.com>
2016-06-14 01:21:20 -0300

    creating state_graph_monthly_spec

---
+++ AbasteceAqui/spec/controllers/state_graph_monthly_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe StateGraphMonthlyController, type: :controller do
+
+  before do
+    @state1 = State.new(id: 1, name: "DISTRITO FEDERAL");
+    @county1 = County.new(id: 1, name: "BRASILIA", state_id: 1)
+    @fuel_research1 = FuelResearch.new(id: 1, date: "2015-04-01", county_id: 1)
+    @fuel_research2 = FuelResearch.new(id: 2, date: "2014-05-01", county_id: 1)
+    @fuel1 = Fuel.new(id: 1, medium_resale_price: 3.0, fuel_type_id: 1, fuel_research_id: 1)
+    @fuel2 = Fuel.new(id: 2, medium_resale_price: 5.0, fuel_type_id: 2, fuel_research_id: 1)
+    @fuel3 = Fuel.new(id: 3, medium_resale_price: 4.0, fuel_type_id: 5, fuel_research_id: 2)
+
+
+    @state1.save
+    @county1.save
+    @fuel_research1.save
+    @fuel_research2.save
+    @fuel1.save
+    @fuel2.save
+    @fuel3.save
+
+  end
+
+
+end
+
+  
