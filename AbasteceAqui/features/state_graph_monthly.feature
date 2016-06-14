4b11ac550d213ee7b8cc0b4a86240f17428db356
Victor Fernandes <victor.hmfd@gmail.com>
2016-06-14 17:49:52 -0300

    fixing functional test of state_graph_monthly feature

--- AbasteceAqui/features/state_graph_monthly.feature
+++ AbasteceAqui/features/state_graph_monthly.feature
@@ -6,5 +6,5 @@ Feature: See statistics
 	Scenario: Success in see the monthly variation graphic of fuel price in a State in a year
 		Given I am in the monthly state chart page
 		When I fill "Escolha o Estado Desejado" with "ALAGOAS"
-		And I fill "Ecolha o Ano Desejado"  with "2013"
+		And I fill 'Ecolha o Ano Desejado'  with "2013"
 		Then the statistics page is load with the monthly variation graphic of fuel price in "ACRE" in the year of "2013"
--- AbasteceAqui/features/step_definitions/state_graph_monthly_steps.rb
+++ AbasteceAqui/features/step_definitions/state_graph_monthly_steps.rb
@@ -2,6 +2,9 @@ Before do
 	@state = State.new(id: 2, name: "ALAGOAS", region_id: 2)
 	@state.save
 
+	fuel_research = FuelResearch.new(id: 1, date: "01-01-2013")
+	fuel_research.save
+
 end
 
 #Scenario 1
@@ -9,17 +12,17 @@ Given(/^I am in the monthly state chart page$/) do
   visit '/statistics/state_monthly'
 end
 
-When (/^I fill "Escolha o Estado Desejado" with "ALAGOAS"$/) do 
+When (/^I fill "Escolha o Estado Desejado" with "ALAGOAS"$/) do
   	select('ALAGOAS', :from => 'select_tag43')
-
 end
 
-When(/^I fill "([^"]*)"  with "([^"]*)"$/) do |year, tag|
+
+When(/^I fill 'Ecolha o Ano Desejado'  with "([^"]*)"$/) do |year|
 	select(year, :from => 'select_tag1')
 	click_on('select_button')
 end
 
+
 Then(/^the statistics page is load with the monthly variation graphic of fuel price in "([^"]*)" in the year of "([^"]*)"$/) do |arg1, arg2|
 	page.should have_content('Estatísticas')
 end
-
