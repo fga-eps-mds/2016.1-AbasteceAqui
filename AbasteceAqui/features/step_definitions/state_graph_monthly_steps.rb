f62d0ce773c692fa1951e459d6cd6fbb654e80fa
Thiago Lima <thiagolima.unb@gmail.com>
2016-06-14 02:34:02 -0300

    adding functional tests

--- AbasteceAqui/features/step_definitions/state_graph_monthly_steps.rb
+++ AbasteceAqui/features/step_definitions/state_graph_monthly_steps.rb
@@ -1,15 +1,25 @@
-# Before do
-# 	@state = State.new
-# 	@state.name = 'ACRE'
-# 	@state.id = 1
-# 	@state.save
-# end
+Before do
+	@state = State.new(id: 2, name: "ALAGOAS", region_id: 2)
+	@state.save
+
+end
 
 #Scenario 1
 Given(/^I am in the monthly state chart page$/) do
   visit '/statistics/state_monthly'
 end
 
-When (/^I fill "Escolha o Estado Desejado" field with "ALAGOAS"$/) do 
-  	select('ALAGOAS', :from => 'select_tag43')	
+When (/^I fill "Escolha o Estado Desejado" with "ALAGOAS"$/) do 
+  	select('ALAGOAS', :from => 'select_tag43')
+
 end
+
+When(/^I fill "([^"]*)"  with "([^"]*)"$/) do |year, tag|
+	select(year, :from => 'select_tag1')
+	click_on('select_button')
+end
+
+Then(/^the statistics page is load with the monthly variation graphic of fuel price in "([^"]*)" in the year of "([^"]*)"$/) do |arg1, arg2|
+	page.should have_content('Estatísticas')
+end
+
