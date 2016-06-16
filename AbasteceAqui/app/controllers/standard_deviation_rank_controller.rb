6f3b9f57d9788012f72967d09fd9e40a62753ca5
Anderson Silva <andersoonn.ams@gmail.com>
2016-06-16 15:35:48 -0300

    creating find_last_research_of_counties method

--- AbasteceAqui/app/controllers/standard_deviation_rank_controller.rb
+++ AbasteceAqui/app/controllers/standard_deviation_rank_controller.rb
@@ -2,13 +2,25 @@ class StandardDeviationRankController < ApplicationController
 
 	def index
 
+		@states = find_states()
+		@researches = find_last_research_of_counties
+
 	end
 
-	def find_states
+	def self.find_states
 
-		states = States.fill_states()
+		#states = State.fill_object_states()
+		states = State.fill_states()
 
 		return states
+
+	end
+
+	def find_last_research_of_counties(counties)
+
+		researches = FuelResearch.fill_object_last_research(counties)
+
+		return researches
 
 	end
 
