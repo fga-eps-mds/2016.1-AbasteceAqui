19e85d8b673b87b9259c9fe553777b5df7792d0f
Marco Kuroiva <marco.antoniorl10@gmail.com>
2016-06-16 19:27:30 -0300

    creating unit test for method find_all_researches of controller

---
+++ AbasteceAqui/spec/controllers/country_graph_monthly_controller_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe CountryGraphMonthlyController, type: :controller do
+
+	before do
+
+	    @fuel_research1 = FuelResearch.new(id: 1, date: "2015-04-01", county_id: 1)
+	    @fuel_research2 = FuelResearch.new(id: 2, date: "2014-05-01", county_id: 1)
+	    @fuel1 = Fuel.new(id: 1, medium_resale_price: 3.0, fuel_type_id: 1, fuel_research_id: 1)
+	    @fuel2 = Fuel.new(id: 2, medium_resale_price: 5.0, fuel_type_id: 2, fuel_research_id: 1)
+	    @fuel3 = Fuel.new(id: 3, medium_resale_price: 4.0, fuel_type_id: 5, fuel_research_id: 2)
+	
+		@fuel_research1.save
+		@fuel_research2.save
+		@fuel1.save
+		@fuel2.save
+		@fuel3.save
+
+	end
+
+	describe "#find_all_researches" do
+		it "should return all researches" do
+			all_researches = controller.find_all_researches()
+
+			expect(all_researches.count).to eq(2)
+		end
+
+	end
+
+end
