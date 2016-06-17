44bbfa13d3a707625358b539032482e8839a1169
Marco Kuroiva <marco.antoniorl10@gmail.com>
2016-06-17 01:22:52 -0300

    creating methods to sorted fuels and generate graph at CountyGraphMonthly controller

--- AbasteceAqui/app/controllers/county_graph_monthly_controller.rb
+++ AbasteceAqui/app/controllers/county_graph_monthly_controller.rb
@@ -36,4 +36,63 @@ class CountyGraphMonthlyController < ApplicationController
 
 	end
 
+	def find_all_fuels_of_county(all_researches_of_county, year_searched)
+
+		@all_fuels = Fuel.find_all_fuels_of_county_of_selected_year(all_researches_of_county, year_searched)
+
+		return @all_fuels
+
+	end
+
+	def find_fuels_of_county_by_month(all_fuels)
+
+		@month_fuels = Fuel.find_fuels_by_month(all_fuels)
+
+		return @month_fuels
+
+	end
+
+	def sorting_fuels_of_county(month_fuels)
+
+		@sorted_fuels = []
+
+		for i in 0..(fuels_month.length-1)
+
+			@sorted_fuels[0] << fuels_month[i]["GASOLINE"]
+			@sorted_fuels[1] << fuels_month[i]["ETHANOL"]
+			@sorted_fuels[2] << fuels_month[i]["DIESEL"]
+
+		end
+
+		return @sorted_fuels
+
+	end
+
+		#Generate the graph of county monthly
+	def generate_monthly_graph_county(sorted_fuels)
+
+		months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+					"Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+
+		titulo = "Preço do combustivel no decorrer do ano - Brasil #{@year_searched}"
+
+		@chart = LazyHighCharts::HighChart.new('graph') do |graph|
+			graph.title(text:  titulo)
+			graph.xAxis(categories: months)
+			graph.series(name: "Preço Da Gasolina", yAxis: 0, data: sorted_fuels[0])
+			graph.series(name: "Preço Do Etanol", yAxis: 0, data: sorted_fuels[1])
+			graph.series(name: "Preço Do Diesel", yAxis: 0, data: sorted_fuels[2])
+
+			graph.yAxis [
+				{title: {text: "Preço Dos Combustíveis", margin: 70} },
+			]
+
+			graph.legend(align: 'right', verticalAlign: 'top', y: 75, x: -50, layout: 'vertical')
+			graph.chart({defaultSeriesType: "line"})
+
+		end
+
+	end
+
+
 end #end of class
