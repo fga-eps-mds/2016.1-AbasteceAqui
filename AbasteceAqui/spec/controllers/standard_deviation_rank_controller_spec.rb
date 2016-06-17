c73ef299439da2d23dbc003a483d931457cc43af
Anderson Silva <andersoonn.ams@gmail.com>
2016-06-16 23:45:17 -0300

    setting up database of unit tests

---
+++ AbasteceAqui/spec/controllers/standard_deviation_rank_controller_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe StandardDeviationRankController, type: :controller do
+	before do
+    @state1 = State.new(id: 1, name: "PARA", region_id: 1)
+    @state2 = State.new(id: 2, name: "MARANHAO", region_id: 2)
+    @state3 = State.new(id: 3, name: "DISTRITO FEDERAL", region_id: 3);
+
+    @county1 = County.new(id: 1, name: "BRASILIA", state_id: 3)
+    @county2 = County.new(id: 2, name: "GAMA", state_id: 3)
+
+
+
+    @fuel_research1 = FuelResearch.new(id:1, date:"2016-01-01", county_id: 1)
+    @fuel_research2 = FuelResearch.new(id:2, date:"2016-02-01", county_id: 2)
+    @fuel_research3 = FuelResearch.new(id:3, date:"2016-03-01", county_id: 1)
+    @fuel_research4 = FuelResearch.new(id:4, date:"2016-03-01", county_id: 2)
+
+    @fuel1 = Fuel.new(id: 1, number_of_gas_station: 4, min_resale_price: 2.199,
+      medium_resale_price: 2.199, max_resale_price: 2.199, resale_standard_deviation: 0.0,
+      min_distribuition_price: 0.0, medium_distribuition_price: 0.0, max_distribuition_price: 0.0,
+      distribuition_standard_deviation: 0.0, fuel_research_id: 1, fuel_type_id: 1)
+
+    @fuel2 = Fuel.new(id: 2, number_of_gas_station: 4, min_resale_price: 2.199,
+      medium_resale_price: 2.199, max_resale_price: 2.199, resale_standard_deviation: 0.0,
+      min_distribuition_price: 0.0, medium_distribuition_price: 0.0, max_distribuition_price: 0.0,
+      distribuition_standard_deviation: 0.0, fuel_research_id: 2, fuel_type_id: 2)
+
+    @fuel3 = Fuel.new(id: 3, number_of_gas_station: 4, min_resale_price: 2.199,
+      medium_resale_price: 2.199, max_resale_price: 2.199, resale_standard_deviation: 0.0,
+      min_distribuition_price: 0.0, medium_distribuition_price: 0.0, max_distribuition_price: 0.0,
+      distribuition_standard_deviation: 0.0, fuel_research_id: 3, fuel_type_id: 3)
+
+    @fuel4 = Fuel.new(id: 4, number_of_gas_station: 4, min_resale_price: 2.199,
+      medium_resale_price: 2.199, max_resale_price: 2.199, resale_standard_deviation: 0.0,
+      min_distribuition_price: 0.0, medium_distribuition_price: 0.0, max_distribuition_price: 0.0,
+      distribuition_standard_deviation: 0.0, fuel_research_id: 4, fuel_type_id: 5)
+
+
+    @county1.save
+    @county2.save
+
+    @fuel_research1.save
+    @fuel_research2.save
+    @fuel_research3.save
+    @fuel_research4.save
+
+    @fuel1.save
+    @fuel2.save
+    @fuel3.save
+    @fuel4.save
+
+  end
+end
