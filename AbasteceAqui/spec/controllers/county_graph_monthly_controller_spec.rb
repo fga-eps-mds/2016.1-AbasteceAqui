1e5ddf293d9b2bdeaf0f5af2113690ee11316dc4
Ygor Galeno <ygor.tgaleno@gmail.com>
2016-06-18 02:16:12 -0300

    creating test of CountyGraphMonthlyController's find_fuels_of_county_by_month, and comparing by content value

--- AbasteceAqui/spec/controllers/county_graph_monthly_controller_spec.rb
+++ AbasteceAqui/spec/controllers/county_graph_monthly_controller_spec.rb
@@ -26,14 +26,14 @@ RSpec.describe CountyGraphMonthlyController, type: :controller do
     max_distribuition_price: 0.0, distribuition_standard_deviation: 0.0,
     fuel_research_id: 1, fuel_type_id: 1)
     fuel2 = Fuel.new(id: 2, number_of_gas_station: 4,
-    min_resale_price: 2.199, medium_resale_price: 2.199,
+    min_resale_price: 2.199, medium_resale_price: 2.299,
     max_resale_price: 2.199, resale_standard_deviation: 0.0,
     min_distribuition_price: 0.0, medium_distribuition_price: 0.0,
     max_distribuition_price: 0.0, distribuition_standard_deviation: 0.0,
     fuel_research_id: 1, fuel_type_id: 2)
 
     fuel3 = Fuel.new(id: 3, number_of_gas_station: 4,
-         min_resale_price: 2.199, medium_resale_price: 2.199,
+         min_resale_price: 2.199, medium_resale_price: 2.399,
          max_resale_price: 2.199, resale_standard_deviation: 0.0,
          min_distribuition_price: 0.0, medium_distribuition_price: 0.0,
          max_distribuition_price: 0.0, distribuition_standard_deviation: 0.0,
@@ -74,6 +74,7 @@ RSpec.describe CountyGraphMonthlyController, type: :controller do
       counties_states_1 = controller.find_all_counties_of_state_searched(states[1])
 
       expect(counties_states_0.count).to eq(1)
+
       expect(counties_states_1.count).to eq(1)
 
     end
@@ -88,6 +89,7 @@ RSpec.describe CountyGraphMonthlyController, type: :controller do
       counties_states_1 = controller.find_all_counties_of_state_searched(states[1])
 
       expect(counties_states_0[0]).to eq("BRASILIA")
+
       expect(counties_states_1[0]).to eq("RIO DE JANEIRO")
 
     end
@@ -103,6 +105,7 @@ RSpec.describe CountyGraphMonthlyController, type: :controller do
       researches_county_rio_de_janeiro = controller.find_all_researches_of_county_searched(county_searched_rio_de_janeiro)
 
       expect(researches_county_brasilia.count).to eq(1)
+
       expect(researches_county_rio_de_janeiro.count).to eq(1)
 
     end
@@ -118,6 +121,7 @@ RSpec.describe CountyGraphMonthlyController, type: :controller do
       researches_county_rio_de_janeiro = controller.find_all_researches_of_county_searched(county_searched_rio_de_janeiro)
 
       expect(researches_county_brasilia[0].id).to eq(1)
+
       expect(researches_county_rio_de_janeiro[0].id).to eq(2)
 
     end
@@ -136,6 +140,7 @@ RSpec.describe CountyGraphMonthlyController, type: :controller do
       years_research_rio_de_janeiro = controller.find_all_years_of_researches(researches_county_rio_de_janeiro)
 
       expect(years_research_brasilia.count).to eq(1)
+
       expect(years_research_rio_de_janeiro.count).to eq(1)
 
     end
@@ -154,6 +159,7 @@ RSpec.describe CountyGraphMonthlyController, type: :controller do
       years_research_rio_de_janeiro = controller.find_all_years_of_researches(researches_county_rio_de_janeiro)
 
       expect(years_research_brasilia[0]).to eq(2015)
+
       expect(years_research_rio_de_janeiro[0]).to eq(2016)
 
     end
@@ -172,6 +178,7 @@ RSpec.describe CountyGraphMonthlyController, type: :controller do
       researches_rio_de_janeiro_year = controller.find_researches_of_selected_year("2016", researches_county_rio_de_janeiro)
 
       expect(researches_brasilia_year.count).to eq(1)
+
       expect(researches_rio_de_janeiro_year.count).to eq(1)
 
     end
@@ -190,6 +197,7 @@ RSpec.describe CountyGraphMonthlyController, type: :controller do
       researches_rio_de_janeiro_year = controller.find_researches_of_selected_year("2016", researches_county_rio_de_janeiro)
 
       expect(researches_brasilia_year[0].id).to eq(1)
+
       expect(researches_rio_de_janeiro_year[0].id).to eq(2)
 
     end
@@ -208,6 +216,7 @@ RSpec.describe CountyGraphMonthlyController, type: :controller do
       fuels_rio_de_janeiro = controller.find_all_fuels_of_county(researches_rio_de_janeiro)
 
       expect(fuels_brasilia.count).to eq(2)
+
       expect(fuels_rio_de_janeiro.count).to eq(1)
 
     end
@@ -227,6 +236,7 @@ RSpec.describe CountyGraphMonthlyController, type: :controller do
 
       expect(fuels_brasilia[0].id).to eq(1)
       expect(fuels_brasilia[1].id).to eq(2)
+
       expect(fuels_rio_de_janeiro[0].id).to eq(3)
 
     end
@@ -250,9 +260,49 @@ RSpec.describe CountyGraphMonthlyController, type: :controller do
       fuels_month_rio_de_janeiro = controller.find_fuels_of_county_by_month(fuels_rio_de_janeiro)
 
       expect(fuels_month_brasilia.count).to eq(12)
+      expect(fuels_month_brasilia[0].count).to eq(3)
+
       expect(fuels_month_rio_de_janeiro.count).to eq(12)
+      expect(fuels_month_rio_de_janeiro[0].count).to eq(3)
 
     end
   end
+
+  describe "#find_fuels_of_county_by_month comparing by content value" do
+    it "should return all fuels sorted by month of research's county of selected year" do
+
+      county_searched_brasilia = "BRASILIA"
+      county_searched_rio_de_janeiro = "RIO DE JANEIRO"
+
+      researches_county_brasilia = controller.find_all_researches_of_county_searched(county_searched_brasilia)
+      researches_county_rio_de_janeiro = controller.find_all_researches_of_county_searched(county_searched_rio_de_janeiro)
+      researches_brasilia = controller.find_researches_of_selected_year("2015", researches_county_brasilia)
+      researches_rio_de_janeiro = controller.find_researches_of_selected_year("2016", researches_county_rio_de_janeiro)
+
+      fuels_brasilia = controller.find_all_fuels_of_county(researches_brasilia)
+      fuels_rio_de_janeiro = controller.find_all_fuels_of_county(researches_rio_de_janeiro)
+
+      fuels_month_brasilia = controller.find_fuels_of_county_by_month(fuels_brasilia)
+      fuels_month_rio_de_janeiro = controller.find_fuels_of_county_by_month(fuels_rio_de_janeiro)
+
+      expect(fuels_month_brasilia[0]["GASOLINE"]).to eq([])
+      expect(fuels_month_brasilia[0]["ETHANOL"]).to eq([])
+      expect(fuels_month_brasilia[0]["DIESEL"]).to eq([])
+
+      expect(fuels_month_brasilia[1]["GASOLINE"]).to eq([2.299])
+      expect(fuels_month_brasilia[1]["ETHANOL"]).to eq([2.199])
+      expect(fuels_month_brasilia[1]["DIESEL"]).to eq([])
+
+      expect(fuels_month_rio_de_janeiro[0]["GASOLINE"]).to eq([])
+      expect(fuels_month_rio_de_janeiro[0]["ETHANOL"]).to eq([])
+      expect(fuels_month_rio_de_janeiro[0]["DIESEL"]).to eq([])
+
+      expect(fuels_month_rio_de_janeiro[1]["GASOLINE"]).to eq([])
+      expect(fuels_month_rio_de_janeiro[1]["ETHANOL"]).to eq([])
+      expect(fuels_month_rio_de_janeiro[1]["DIESEL"]).to eq([2.399])
+
+    end
+  end
+
 
 end
