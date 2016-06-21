764ef149e3f28140ac3ac3b65a3129423ad7e56e
Thiago Lima <thiagolima.unb@gmail.com>
2016-06-21 00:00:05 -0300

    refactoring county_graph_monthly_controller to reduce simmilar code

--- AbasteceAqui/app/controllers/county_graph_monthly_controller.rb
+++ AbasteceAqui/app/controllers/county_graph_monthly_controller.rb
@@ -21,9 +21,11 @@ class CountyGraphMonthlyController < ApplicationController
 			find_researches_of_selected_year(params[:year_selected], @all_researches_of_county)
 			find_all_fuels_of_county(@researches_of_year)
 			find_fuels_of_county_by_month(@all_fuels)
-			sorting_gasoline_of_county(@month_fuels)
-			sorting_ethanol_of_county(@month_fuels)
-			sorting_diesel_of_county(@month_fuels)
+
+			@sorted_gasoline_array = sorting_a_fuel_of_county(@month_fuels, "GASOLINE")
+			@sorted_ethanol_array = sorting_a_fuel_of_county(@month_fuels, "ETHANOL")
+			@sorted_diesel_array = sorting_a_fuel_of_county(@month_fuels, "DIESEL")
+
 			generate_monthly_graph_county(@sorted_gasoline_array, @sorted_ethanol_array, @sorted_diesel_array)
 		else
 			#do nothing
@@ -101,62 +103,22 @@ class CountyGraphMonthlyController < ApplicationController
 	end
 
 	# receive a array of hash(name of fuel as key) separeted by months
-	# sorting gasoline hash in array
-	def sorting_gasoline_of_county(fuels_month)
+	# sorting a given fuel hash in array
+	def sorting_a_fuel_of_county(fuels_month, fuel_name)
 
-		@sorted_gasoline_array = []
+		sorted_fuel_array = []
 
 		for i in 0..(fuels_month.length-1)
 
-			if fuels_month[i]["GASOLINE"] != []
-				@sorted_gasoline_array[i] = fuels_month[i]["GASOLINE"]
+			if fuels_month[i][fuel_name] != []
+				sorted_fuel_array[i] = fuels_month[i][fuel_name]
 			else
-				@sorted_gasoline_array[i] = nil
+				sorted_fuel_array[i] = nil
 			end
 
 		end
 
-		@sorted_gasoline_array
-
-	end
-
-	# receive a array of hash(name of fuel as key) separeted by months
-	# sorting ethanol hash in array
-	def sorting_ethanol_of_county(fuels_month)
-
-		@sorted_ethanol_array = []
-
-		for i in 0..(fuels_month.length-1)
-
-			if fuels_month[i]["ETHANOL"] != []
-				@sorted_ethanol_array[i] = fuels_month[i]["ETHANOL"]
-			else
-				@sorted_ethanol_array[i] = nil
-			end
-
-		end
-
-		@sorted_ethanol_array
-
-	end
-
-	# receive a array of hash(name of fuel as key) separeted by months
-	# sorting diesel hash in array
-	def sorting_diesel_of_county(fuels_month)
-
-		@sorted_diesel_array = []
-
-		for i in 0..(fuels_month.length-1)
-
-			if fuels_month[i]["DIESEL"] != []
-				@sorted_diesel_array[i] = fuels_month[i]["DIESEL"]
-			else
-				@sorted_diesel_array[i] = nil
-			end
-
-		end
-
-		@sorted_diesel_array
+		return sorted_fuel_array
 
 	end
 
