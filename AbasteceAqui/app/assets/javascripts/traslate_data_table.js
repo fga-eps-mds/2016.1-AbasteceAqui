0d692417a671aece16fced0c5bd1423838a44165
Marcelo <marceloabk@hotmail.com>
2016-06-21 12:37:23 -0300

    adding data-table translate to a separated js

---
+++ AbasteceAqui/app/assets/javascripts/traslate_data_table.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+    $('#data-table').DataTable( {
+        "bSort": false,
+        "language": {
+          url: "//cdn.datatables.net/plug-ins/1.10.11/i18n/Portuguese-Brasil.json"
+        }
+    });
+});
