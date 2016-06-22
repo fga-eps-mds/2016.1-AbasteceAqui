94c89e008f39ff9a2cefa7c8f9f18bdc7484cb26
Ygor Galeno <ygor.tgaleno@gmail.com>
2016-06-22 10:59:21 -0300

    fixing functional test of country_graph with new dropdown

--- AbasteceAqui/features/step_definitions/country_graph_monthly_steps.rb
+++ AbasteceAqui/features/step_definitions/country_graph_monthly_steps.rb
@@ -3,8 +3,9 @@ Given(/^I am in the country_monthly page$/) do
   visit '/statistics/country_monthly'
 end
 
-When(/^I fill "Escolha o ano Desejado" field with "2013"$/) do 
-  select('2013', :from => 'select_tag')
+When(/^I fill "Escolha o ano Desejado" field with "2013"$/) do
+  find(:xpath, '//span[@id="year_tagSelectBoxIt"]').click
+  find('.selectboxit-option[data-val="2013"]').click
 end
 
 Then(/^the country page is loaded with the monthly variation graphic of fuel price in 'Brasil'$/) do
